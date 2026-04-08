d8a3201998b6056a5aacc4a6bf7386816019b8de
ardi <ardichuan21@gmail.com>
2026-04-08 18:52:49 +0700

    Delete backend directory

--- backend/Models/Category.cs
+++
@@ -1,7 +0,0 @@
-namespace AponkRed.Api.Models;
-
-public class Category
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = "";
-}
--- backend/Models/Customer.cs
+++
@@ -1,9 +0,0 @@
-namespace AponkRed.Api.Models;
-
-public class Customer
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = "";
-    public string? Phone { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
